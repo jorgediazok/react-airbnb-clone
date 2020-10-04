fb388e5cfbfd36f4eb8a799c3204cad356947aee
Jorge <jorgediazok@gmail.com>
2020-10-04 19:32:33 -0300

    footer complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home';
 import Header from './Header';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <Header />
       <Home />
+      <Footer />
 
       {/*Home*/}
       {/*Header*/}
