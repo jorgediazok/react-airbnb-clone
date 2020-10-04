7af285290f9131d44aa50dcf4eaae24baf7da262
Jorge <jorgediazok@gmail.com>
2020-10-04 01:16:45 -0300

    refactoring banner

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
+import './Banner.css';
 import './Home.css';
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home Component</h1>
+      <Banner />
     </div>
   );
 }
